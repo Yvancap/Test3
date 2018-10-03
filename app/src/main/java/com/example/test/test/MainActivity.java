477ddc5d3206c130bab476814921c7cf3bab4df6
Basile Bruhat <basile.bruhat@etudiant.univ-rennes1.fr>
2018-10-03 17:34:56 +0200

    Ajout fonction test

--- app/src/main/java/com/example/test/test/MainActivity.java
+++ app/src/main/java/com/example/test/test/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.test.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test();
+    }
+
+    public void test(){
+        Log.d("mon app", "ceci est un test");
     }
 }
