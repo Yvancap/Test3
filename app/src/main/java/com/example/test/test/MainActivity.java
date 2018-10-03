2c24b089808415b17f2bf800284536241e27917b
yvan ulas <yulas@enssat.fr>
2018-10-03 19:48:38 +0200

    modif3

--- app/src/main/java/com/example/test/test/MainActivity.java
+++ app/src/main/java/com/example/test/test/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         test();
-        Log.d("Monapp","Fais chié");
+
 
     }
 
